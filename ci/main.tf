f6e8128dfbfd7ee9473259f06c634c5eae79eab9
Benjamin Sherwood <bsherwood@lemongrassconsulting.com>
2019-06-07 15:11:45 +0100

    Terraform and circleci config

--- ci/main.tf
+++ ci/main.tf
@@ -9,24 +9,24 @@ data "github_team" "dataworks" {
   slug = "dataworks"
 }
 
-resource "github_repository" "docker-cp-confluent-kafka-connect" {
-  name        = "docker-cp-confluent-kafka-connect"
-  description = "Confluent Kafka Connect Docker image with SSL certifciate signing "
+resource "github_repository" "docker-python-boto-behave" {
+  name        = "docker-python-boto-behave"
+  description = "Docker python/3.7-alpine image with Boto and Behave."
 
   allow_merge_commit = false
   default_branch     = "master"
   has_issues         = true
 }
 
-resource "github_team_repository" "docker-cp-confluent-kafka-connect-dataworks" {
-  repository = "${github_repository.docker-cp-confluent-kafka-connect.name}"
+resource "github_team_repository" "docker-python-boto-behave-dataworks" {
+  repository = "${github_repository.docker-python-boto-behave.name}"
   team_id    = "${data.github_team.dataworks.id}"
   permission = "admin"
 }
 
 resource "github_branch_protection" "master" {
-  branch         = "${github_repository.docker-cp-confluent-kafka-connect.default_branch}"
-  repository     = "${github_repository.docker-cp-confluent-kafka-connect.name}"
+  branch         = "${github_repository.docker-python-boto-behave.default_branch}"
+  repository     = "${github_repository.docker-python-boto-behave.name}"
   enforce_admins = true
 
   required_status_checks {
@@ -42,5 +42,5 @@ resource "github_branch_protection" "master" {
 resource "github_issue_label" "wip" {
   color      = "f4b342"
   name       = "WIP"
-  repository = "${github_repository.docker-cp-confluent-kafka-connect.name}"
+  repository = "${github_repository.docker-python-boto-behave.name}"
 }
